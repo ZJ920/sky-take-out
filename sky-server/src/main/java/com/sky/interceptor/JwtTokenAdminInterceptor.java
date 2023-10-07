351f8e85985d47331695a7c1c2d2f8f4ab51ae83
ZJ920 <1622781044@qq.com>
2023-10-07 23:13:23 +0800

    员工添加

--- sky-server/src/main/java/com/sky/interceptor/JwtTokenAdminInterceptor.java
+++ sky-server/src/main/java/com/sky/interceptor/JwtTokenAdminInterceptor.java
@@ -11,6 +11,8 @@ import org.springframework.web.method.HandlerMethod;
 import org.springframework.web.servlet.HandlerInterceptor;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
+import java.util.Map;
+import java.util.function.BiConsumer;
 
 /**
  * jwt令牌校验的拦截器
@@ -31,22 +33,29 @@ public class JwtTokenAdminInterceptor implements HandlerInterceptor {
      * @return
      * @throws Exception
      */
+    @Override
     public boolean preHandle(HttpServletRequest request, HttpServletResponse response, Object handler) throws Exception {
         //判断当前拦截到的是Controller的方法还是其他资源
         if (!(handler instanceof HandlerMethod)) {
+            ///admin/employee/login 是
+            ///admin/employee/ 不是
+            //使用反射机制获取路径上的方法名称
             //当前拦截到的不是动态方法，直接放行
             return true;
         }
 
         //1、从请求头中获取令牌
+        //jwtProperties.getAdminTokenName() = admin-token-name: token(yml配置文件中)
         String token = request.getHeader(jwtProperties.getAdminTokenName());
+        System.out.println("token值："+token);
 
         //2、校验令牌
         try {
             log.info("jwt校验:{}", token);
             Claims claims = JwtUtil.parseJWT(jwtProperties.getAdminSecretKey(), token);
             Long empId = Long.valueOf(claims.get(JwtClaimsConstant.EMP_ID).toString());
-            log.info("当前员工id：", empId);
+            log.info("当前员工id：{}", empId);
+            System.out.println("令牌校验通过，放行");
             //3、通过，放行
             return true;
         } catch (Exception ex) {
--- sky-server/src/main/java/com/sky/service/impl/EmployeeServiceImpl.java
+++ sky-server/src/main/java/com/sky/service/impl/EmployeeServiceImpl.java
@@ -1,7 +1,9 @@
 package com.sky.service.impl;
 
 import com.sky.constant.MessageConstant;
+import com.sky.constant.PasswordConstant;
 import com.sky.constant.StatusConstant;
+import com.sky.dto.EmployeeDTO;
 import com.sky.dto.EmployeeLoginDTO;
 import com.sky.entity.Employee;
 import com.sky.exception.AccountLockedException;
@@ -9,9 +11,12 @@ import com.sky.exception.AccountNotFoundException;
 import com.sky.exception.PasswordErrorException;
 import com.sky.mapper.EmployeeMapper;
 import com.sky.service.EmployeeService;
+import org.springframework.beans.BeanUtils;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 import org.springframework.util.DigestUtils;
+
+import java.time.LocalDateTime;
 
 @Service
 public class EmployeeServiceImpl implements EmployeeService {
@@ -53,6 +58,31 @@ public class EmployeeServiceImpl implements EmployeeService {
 
         //3、返回实体对象
         return employee;
+    }
+
+    /**
+     * 新增员工
+     * @param employeeDTO
+     */
+    public void save(EmployeeDTO employeeDTO) {
+
+        Employee employee = new Employee();
+        //复制对象
+        BeanUtils.copyProperties(employeeDTO,employee);
+        //密码加密
+        String password = DigestUtils.md5DigestAsHex(PasswordConstant.DEFAULT_PASSWORD.getBytes());
+        employee.setPassword(password);
+        //设置状态
+        employee.setStatus(StatusConstant.ENABLE);
+        //创建、修改时间
+        employee.setCreateTime(LocalDateTime.now());
+        employee.setUpdateTime(LocalDateTime.now());
+        //设置创建、修改人
+        //TODO 修改创建、修改人数据
+        employee.setCreateUser(10L);
+        employee.setUpdateUser(10L);
+
+        employeeMapper.insert(employee);
     }
 
 }
